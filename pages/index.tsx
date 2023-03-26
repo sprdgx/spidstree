ce669004f3173e364bcad8fc5e8de6cf6e771f2d
sprdgx <spardagx@gmail.com>
2023-03-26 20:48:17 +0800

    okayy

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import { useRouter } from "next/router";
 
 export default function Index () {
@@ -8,10 +9,19 @@ export default function Index () {
     router.push('/signup') 
 }
     return (
-        <div className='bg-blue-300 w-10/12 lg:w-1/2 rounded-3xl flex flex-col  items-center pt-[1.5rem] lg:mx-[20rem] mt-[8.5rem] mx-[2rem]'>
+
+      <>
+      <Head>
+        <title>SpidsTree</title>
+        <meta name="description" content="Save Your Links Here " />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="https://raw.githubusercontent.com/sprdgx/Photos/main/SpidsTree.png" />
+      </Head>
+      <div className='bg-blue-300 w-10/12 lg:w-1/2 rounded-3xl flex flex-col  items-center pt-[1.5rem] lg:mx-[20rem] mt-[8.5rem] mx-[2rem]'>
           <h3 className=" lg:text-6xl text-3xl text-center w-full  text-white font-logo pt-3">Welcime To SpidsTrEE</h3>
           <h3 className='text-md text-black  font-mono text-center mt-3 bg-gray-200 rounded-xl py-[1rem] px-[1rem] mx-[1rem] my-[1rem]'> Share Social Media and Websites Links in One Link With SPIDSTREE</h3>
           <h3 onClick={SignUpFirst} className=" cursor-pointer lg:text-3xl text-xl text-center w-full  text-white font-mono py-[1rem] px-[1rem] mx-[1rem] my-[1rem] pt-3">Get Started By Signing Up First ^^ Click Here </h3>
-        </div>
+      </div>
+      </>
     )
 }
--- pages/logedin.tsx
+++ pages/logedin.tsx
@@ -1,9 +1,19 @@
+import Head from "next/head";
+
 export default function LogedIn () {
     return (
-        <div className='bg-blue-300 w-10/12 lg:w-1/2 rounded-3xl flex flex-col  items-center  lg:mx-[20rem] mt-[10rem] mx-[2rem]'>
+      <>
+      <Head>
+        <title>SpidsTree</title>
+        <meta name="description" content="Save Your Links Here " />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="https://raw.githubusercontent.com/sprdgx/Photos/main/SpidsTree.png" />
+      </Head>
+      <div className='bg-blue-300 w-10/12 lg:w-1/2 rounded-3xl flex flex-col  items-center  lg:mx-[20rem] mt-[10rem] mx-[2rem]'>
           <h3 className=" lg:text-6xl text-5xl text-center w-full  text-white font-logo pt-[2rem]">SpiDsTrEE</h3>
           <h3 className=" lg:text-3xl text-5xl text-center w-full  text-green-300 font-logo pt-3">Thank You For Signing</h3>
           <h3 className='text-sm text-black  font-mono text-center mt-3 bg-gray-200 rounded-xl py-[1rem] lg:px-[1rem] px-[0.5rem] mx-[1rem] my-[1rem]'> For Adding Links, May Please Go To Your Generated Dashboard Which Will Contain Your Username You Chosed As A Path For Example : https://spidstree.vercel.app/username </h3>
         </div>
+      </>
     )
 }
