78b0c3dbdd632bd979ec882afb24bb4ad83c5e36
sprdgx <spardagx@gmail.com>
2023-05-20 02:31:38 +0800

    PWA

--- next.config.js
+++ next.config.js
@@ -1,9 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  images : {
-    domains:['https://raw.githubusercontent.com/'],
-  }
-}
 
-module.exports = nextConfig
+const withPWA = require('next-pwa');
+
+module.exports = withPWA({
+  
+    dest: 'public',
+    register: true,
+    skipWaiting: true,
+    disable: process.env.NODE_ENV === 'development',
+});
